9253c430b8e8c539bb4fc1daa7fb4faf6846148f
Dell <Dell@DESKTOP-USPFAQA>
2025-04-18 17:17:09 +0530

    By Aarti

---
+++ GitDemo/src/main/java/com/app/GitDemo/main/Demo.java
@@ -0,0 +1,14 @@
+package com.app.GitDemo.main;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("This is main method..");
+	}
+	
+	public void m1()
+	{
+		System.out.println("This is m1 method..");
+	}
+
+}
